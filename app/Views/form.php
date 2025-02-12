8dccd4c421e6b60cb2c192964096d3a31a23e3f7
Diego17 <diegoalbertocastropastor@gmail.com>
2025-02-12 12:59:01 -0500

    ``` feat: add new input field styles for improved accessibility and user experience ```

--- app/Views/form.php
+++ app/Views/form.php
@@ -60,6 +60,44 @@
                             </div>
                         </div>
                     </div>
+                    <div id="orador-info" style="display: none;">
+                        <div class="form-group">
+                            <label for="titular" style="color: #000000; font-size: 1.2em;">Su participación sera a titulo...</label><br>
+                            <div class="form-check form-check-inline">
+                                <input type="radio" class="form-check-input" id="personal" name="titular" value="PERSONAL">
+                                <label class="form-check-label" for="personal" style="color: #000000; font-size: 1.2em;">Personal</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input type="radio" class="form-check-input" id="organizacion" name="titular" value="ORGANIZACION">
+                                <label class="form-check-label" for="organizacion" style="color: #000000; font-size: 1.2em;">Organización</label>
+                            </div>
+                        </div>
+                        <div id="organizacion-info" style="display: none;">
+                            <div class="form-group text">
+                                <input type="text" class="form-part" name="ruc" pattern="\d{11}" title="Por favor, ingresar un RUC válido">
+                                <label for="ruc">Nº RUC</label>
+                            </div>
+                            <div class="form-group text">
+                                <input type="text" class="form-part" name="nombre_organizacion" maxlength="80">
+                                <label for="nombre_organizacion">Nombre de la Organización</label>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="eje">EJE Tematico</label>
+                            <select name="eje" class="form-control" style="background-color: transparent; border: none; border-bottom: 1px solid #000000; width: 100%; margin-top: 5px;">
+                                <option value="" disabled selected>Seleccione un EJE Tematico</option>
+                                <option value="Seguridad Ciudadana">Seguridad Ciudadana</option>
+                                <option value="Limpieza Pública">Limpieza Pública</option>
+                                <option value="Infraestructura">Infraestructura</option>
+                                <option value="Programas Sociales">Programas Sociales</option>
+                                <option value="Institucionalidad">Institucionalidad</option>
+                            </select>
+                        </div>
+                        <div class="form-group text">
+                            <input type="text" class="form-part" name="pregunta" pattern="[A-Za-z\s]{1,200}" title="Por favor, ingresar solo letras (máximo 200 caracteres)">
+                            <label for="pregunta">Pregunta</label>
+                        </div>
+                    </div>
                     <div id="submit-buttons">
                         <button type="submit" class="btn btn-primary btn-form" id="submit-button" style="display: block;">Enviar</button>
                         <button type="button" class="btn btn-primary btn-form" id="next-button" style="display: none;" disabled>Siguiente</button>
@@ -68,47 +106,6 @@
             </div>
         </div>
     </main>
-    <form action="<?= base_url('procesar_formulario') ?>" method="post" class="container mt-5" style="max-width: 600px; margin: auto; padding: 10px; border-radius: 10px; border: 2px solid #002F59; background-color: #FFFFFF;">
-        <div id="orador-info" style="display: none;">
-            <div class="form-group">
-                <label for="titular" style="color: #000000; font-size: 1.2em;">Su participación sera a titulo...</label><br>
-                <div class="form-check form-check-inline">
-                    <input type="radio" class="form-check-input" id="personal" name="titular" value="PERSONAL">
-                    <label class="form-check-label" for="personal" style="color: #000000; font-size: 1.2em;">Personal</label>
-                </div>
-                <div class="form-check form-check-inline">
-                    <input type="radio" class="form-check-input" id="organizacion" name="titular" value="ORGANIZACION">
-                    <label class="form-check-label" for="organizacion" style="color: #000000; font-size: 1.2em;">Organización</label>
-                </div>
-            </div>
-            <div id="organizacion-info" style="display: none;">
-                <div class="form-group">
-                    <label for="ruc" style="color: #000000; font-size: 1.2em;">Nº RUC</label>
-                    <input type="text" class="form-control" name="ruc" pattern="\d{11}" title="Por favor, ingresar un RUC válido" style="background-color: transparent; border: none; border-bottom: 1px solid #000000; width: 100%; margin-top: 5px;">
-                </div>
-                <div class="form-group">
-                    <label for="nombre_organizacion" style="color: #000000; font-size: 1.2em;">Nombre de la Organización</label>
-                    <input type="text" class="form-control" name="nombre_organizacion" maxlength="80" style="background-color: transparent; border: none; border-bottom: 1px solid #000000; width: 100%; margin-top: 5px;">
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="eje" style="color: #000000; font-size: 1.2em;">EJE Tematico</label>
-                <select name="eje" class="form-control" style="background-color: transparent; border: none; border-bottom: 1px solid #000000; width: 100%; margin-top: 5px;">
-                    <option value="" disabled selected>Seleccione un EJE Tematico</option>
-                    <option value="Seguridad Ciudadana">Seguridad Ciudadana</option>
-                    <option value="Limpieza Pública">Limpieza Pública</option>
-                    <option value="Infraestructura">Infraestructura</option>
-                    <option value="Programas Sociales">Programas Sociales</option>
-                    <option value="Institucionalidad">Institucionalidad</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="pregunta" style="color: #000000; font-size: 1.2em;">Pregunta</label>
-                <input type="text" class="form-control" name="pregunta" pattern="[A-Za-z\s]{1,200}" title="Por favor, ingresar solo letras (máximo 200 caracteres)" style="background-color: transparent; border: none; border-bottom: 1px solid #000000; width: 100%; margin-top: 5px;">
-            </div>
-        </div>
-
-    </form>
 
     <!-- Bootstrap JS and dependencies -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
