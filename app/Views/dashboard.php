785fd60e80369c559c79c34e5d429be31578cf08
Diego17 <diegoalbertocastropastor@gmail.com>
2025-02-11 23:08:20 -0500

    Remove Tailwind CSS configuration and dependencies

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -169,6 +169,7 @@
 
     <!-- SCRIPTS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     <!-- -->
 
 </body>
