c460fb60893dd7efabe5bcefe4cd407fa3a43a58
Julian Burga Bracamonte <julianburgabracamonte@gmail.com>
2025-02-16 11:17:54 -0500

    Create Preguntas_seleccionadas.php

---
+++ app/Models/Preguntas_seleccionadas.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+class Preguntas_seleccionadas extends \CodeIgniter\Model
+{
+    protected $table = 'preguntas_seleccionadas';
+    protected $primaryKey = 'id_pregunta_seleccionada';
+    protected $useAutoIncrement = false;
+    protected $returnType = 'array';
+    protected $useSoftDeletes = false;
+    protected $allowedFields = ['id_pregunta_seleccionada', 'id_eje_seleccionado', 'id_pregunta'];
+    
+    protected $useTimestamps = false;
+    protected $updatedField  = false;
+    
+}
