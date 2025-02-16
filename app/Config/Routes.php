5ae2fa5317071034530034a8ab1c2718a6164751
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2025-02-15 22:41:48 -0500

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,8 +6,12 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+//Rutas de form
 $routes->get('form', 'FormController::index');
 $routes->post('procesar_formulario', 'FormController::procesar_formulario');
-$routes->get('admin', 'adminController::index');
+//Rutas de admin
+$routes->get('admin', 'adminController::buscar_eje');
+$routes->post('crear_eje', 'adminController::crear_eje');
+//Rutas de api
 $routes->get('api/dni/(:num)', 'ConsultaApi::buscarDNI/$1');
 $routes->get('api/ruc/(:num)', 'ConsultaApi::buscarRUC/$1');
