c593e468f6712162e49b54113c27c033a136baba
Julian Burga Bracamonte <julianburgabracamonte@gmail.com>
2025-02-16 11:17:52 -0500

    Create Ejes_SeleccionadosModel.php

---
+++ app/Models/Ejes_SeleccionadosModel.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace App\Models;
+
+class Ejes_SeleccionadosModel extends \CodeIgniter\Model
+{
+    protected $table = 'ejes_seleccionados';
+    protected $primaryKey = 'id_eje_seleccionado';
+    protected $useAutoIncrement = false;
+    protected $returnType = 'array';
+    protected $useSoftDeletes = false;
+    protected $allowedFields = ['id_eje_seleccionado', 'id_rendicion', 'id_eje', 'cantidad_preguntas'];
+    
+    protected $useTimestamps = false;
+    protected $updatedField  = false;
+    
+}
