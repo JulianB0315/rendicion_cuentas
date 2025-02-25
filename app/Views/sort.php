47fe18d67757e6ebac1b0dbcf5946bef838b225c
Diego17 <diegoalbertocastropastor@gmail.com>
2025-02-24 19:46:06 -0500

    Update page title in sort view for clarity

--- app/Views/sort.php
+++ app/Views/sort.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Sorteo de Preguntas - <?= $eje['tematica'] ?></title>
+    <title>Selección de Preguntas - <?= $eje['tematica'] ?></title>
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
         rel="stylesheet"
