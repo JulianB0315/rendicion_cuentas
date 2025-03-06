075a08d646f30bc99e5e7ea6c756f7761a7f80f6
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-05 19:15:29 -0500

    Implement DNI validation and error handling in login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,19 +1,69 @@
-const btnToggle = document.getElementById('togglePassword');
-const passwordInput = document.getElementById('password');
-const dniInput = document.getElementById('dni');
-const error = document.getElementById('dni-error');
-const nombreInput = document.getElementById('nombre');
-const submitBtn = document.getElementById('submit-button');
+const btnToggle = document.getElementById("togglePassword");
+const passwordInput = document.getElementById("password");
+const dniInput = document.getElementById("dni");
+const error = document.getElementById("dni-error");
+const nombreInput = document.getElementById("nombre");
+const submitBtn = document.getElementById("submit-button");
 
-btnToggle.addEventListener('click', () => {
-    const icon = btnToggle.querySelector('i');
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        icon.classList.add('fa-eye');
-        icon.classList.remove('fa-eye-slash');
-    } else {
-        passwordInput.type = 'password';
-        icon.classList.add('fa-eye-slash');
-        icon.classList.remove('fa-eye');
-    }
+submitBtn.disabled = true;
+
+searchPerson = async () => {
+	const dni = dniInput.value;
+	const regex = /^\d{8}$/;
+	nombreInput.value = "";
+	error.innerHTML = "";
+
+	if (dni.length < 8) {
+		error.innerHTML = "El DNI debe tener 8 dígitos";
+		nombreInput.value = "";
+		return;
+	}
+
+	if (!regex.test(dni)) {
+		error.innerHTML = "El DNI debe contener solo números";
+		nombreInput.value = "";
+		return;
+	}
+
+	try {
+		const response = await fetch(
+			`http://localhost/rendicion_cuentas/public/api/dni/${dni}`
+		);
+		const data = await response.json();
+
+		if (response.ok && dni.length === 8) {
+			const persona = Array.isArray(data) ? data[0] : data;
+            console.log(persona);
+			if (!persona || !persona.nombres) {
+				error.innerHTML = "No se encontró ninguna persona con ese DNI";
+				nombreInput.value = "";
+				return;
+			}
+			const nombre = `${persona.nombres} ${persona.apellido_paterno} ${persona.apellido_materno}`;
+			if (nombre === undefined) {
+				errorDniMsg.innerHTML = "No se encontró ninguna persona con ese DNI";
+				nombreInput.value = "";
+				return;
+			}
+			asistenciaBtn.disabled = false;
+		} else {
+			error.innerHTML = "No se encontró ninguna persona con ese DNI";
+		}
+	} catch (error) {
+		console.error("Error fetching JSON:", error);
+		error.innerHTML = "Error al buscar la persona";
+	}
+};
+
+btnToggle.addEventListener("click", () => {
+	const icon = btnToggle.querySelector("i");
+	if (passwordInput.type === "password") {
+		passwordInput.type = "text";
+		icon.classList.add("fa-eye");
+		icon.classList.remove("fa-eye-slash");
+	} else {
+		passwordInput.type = "password";
+		icon.classList.add("fa-eye-slash");
+		icon.classList.remove("fa-eye");
+	}
 });
