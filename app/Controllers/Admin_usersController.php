40d69e32ea7a44f84b0d36fe343c1df718a9c51b
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-07 15:07:05 -0500

    Fetch and display admin users in the admin users view

--- app/Controllers/Admin_usersController.php
+++ app/Controllers/Admin_usersController.php
@@ -1,15 +1,19 @@
-<?php 
+<?php
 
 namespace App\Controllers;
 
-// Aqui las tablas
+use App\Models\AdministradoresModel;
 
 class Admin_UsersController extends BaseController
 {
+    private $AdministradoresModel;
+    function __construct()
+    {
+        $this->AdministradoresModel = new AdministradoresModel();
+    }
     public function index()
     {
-        return view('admin_users');
+        $admins = $this->AdministradoresModel->select('dni_admin, nombres_admin, categoria_admin')->where('categoria_admin', 'admin')->findAll();
+        return view('admin_users', ['admins' => $admins]);
     }
 }
-
-?>
