482befff4631991081266150256ab637b54b88ef
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-07 15:16:09 -0500

    Add custom script for admin users view

--- app/Views/admin_users.php
+++ app/Views/admin_users.php
@@ -122,8 +122,8 @@
             </div>
         </div>
     </main>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url('scripts/sudoadm.js') ?>"></script>
 </body>
 
 </html>
