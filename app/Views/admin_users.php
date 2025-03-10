4ea77dfc7814d6cd005a1cf96774a4ea532f1d67
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-10 12:07:57 -0500

    Add editAdmin.js script to admin users view

--- app/Views/admin_users.php
+++ app/Views/admin_users.php
@@ -174,6 +174,7 @@
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="<?= base_url('js/login.js') ?>"></script>
+    <script src="<?= base_url('js/editAdmin.js') ?>"></script>
 </body>
 
 </html>
