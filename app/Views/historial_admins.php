3ced470271cae268e94a516aba92c4bc1ebbaef9
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-12 17:02:35 -0500

    Update historial_admins.php: change language to Spanish and enhance styling with additional CSS links

--- app/Views/historial_admins.php
+++ app/Views/historial_admins.php
@@ -1,9 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Historial de cambios - Administradores</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+        crossorigin="anonymous" />
+    <link
+        rel="stylesheet"
+        href="https://site-assets.fontawesome.com/releases/v6.7.2/css/all.css" />
+    <link rel="stylesheet" href="<?= base_url('styles/index.css') ?>" />
+    <link rel="stylesheet" href="<?= base_url('styles/admin.css') ?>" />
+    <link rel="stylesheet" href="<?= base_url('styles/login.css') ?>" />
+    <link rel="stylesheet" href="<?= base_url("styles/form.css") ?>" />
+    <link rel="stylesheet" href="<?= base_url("styles/dashboard.css") ?>" />
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Encode+Sans+Semi+Expanded:wght@100;200;300;400;500;600;700;800;900&family=Asap:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet" />
 </head>
 <body>
     hola mundooooooooooooooooooo
