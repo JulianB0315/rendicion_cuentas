be25f7fc1434bd2bf4bb959ce40ceaa4d8f329ca
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-12 12:31:20 -0500

    Add methods to retrieve complete history and register admin actions in HistorialAdminModel

--- app/Models/HistorialAdminModel.php
+++ app/Models/HistorialAdminModel.php
@@ -21,6 +21,32 @@ class HistorialAdminModel extends Model
 
     protected $useTimestamps = false;
     protected $updatedField  = false;
-}
 
-?>
+    public function getHistorialCompleto()
+    {
+        return $this->db->table($this->table . 'h')
+            ->select('
+                h.*,
+                a.nombres_admin as admin_afectado,
+                a.categoria_admin as categoria_admin,
+                r.nombres_admin as realizado_por,
+                r.categoria_admin as categoria_realizado_por
+            ')
+            ->join('administradores a', 'a.dni_admin = h.dni_admin')
+            ->join('administradores r', 'r.dni_admin = h.realizado_por')
+            ->orderBy('h.fecha_accion', 'DESC')
+            ->get()
+            ->getResultArray();
+    }
+    public function registrarAccion($dniAdmin, $accion, $motivo)
+    {
+        $data = [
+            'dni_admin' => $dniAdmin,
+            'accion' => $accion,
+            'motivo' => $motivo,
+            'realizado_por' => session()->get('dni_admin'),
+            'fecha_accion' => date('Y-m-d H:i:s')
+        ];
+        $this->insert($data);
+    }
+}
