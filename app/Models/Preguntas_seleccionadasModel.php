6a60c88f310fde8b087709fe9e6b601880f245de
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2025-04-14 18:47:19 -0500

    Update Preguntas_seleccionadasModel.php

--- app/Models/Preguntas_seleccionadasModel.php
+++ app/Models/Preguntas_seleccionadasModel.php
@@ -5,13 +5,39 @@ namespace App\Models;
 class Preguntas_seleccionadasModel extends \CodeIgniter\Model
 {
     protected $table = 'preguntas_seleccionadas';
-    protected $primaryKey = 'id_pregunta_seleccionada';
+    protected $primaryKey = 'id';
     protected $useAutoIncrement = false;
     protected $returnType = 'array';
     protected $useSoftDeletes = false;
-    protected $allowedFields = ['id_pregunta_seleccionada', 'id_eje_seleccionado', 'id_pregunta'];
-    
+    protected $allowedFields =
+    [
+        'id',
+        'id_eje_seleccionado',
+        'id_pregunta'
+    ];
+
+    // Dates
     protected $useTimestamps = false;
-    protected $updatedField  = false;
-    
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
 }
