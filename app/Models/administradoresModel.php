61ec01fde324d0c55d23dbd9356b0a383ae6695f
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2025-04-14 18:46:45 -0500

    Update administradoresModel.php

--- app/Models/administradoresModel.php
+++ app/Models/administradoresModel.php
@@ -6,21 +6,42 @@ class administradoresModel extends \CodeIgniter\Model
 {
     protected $table = 'administradores';
     protected $primaryKey = 'dni_admin';
-    protected $useAutoIncrement = true;
+    protected $useAutoIncrement = false;
     protected $returnType = 'array';
     protected $useSoftDeletes = false;
-    protected $allowedFields = [
+    protected $protectFields = true;
+    protected $allowedFields =
+    [
         'dni_admin',
         'nombres_admin',
         'password',
         'categoria_admin',
         'estado',
-        // 'motivo_deshabilitado',
-        // 'fecha_deshabilitado',
-        // 'deshabilitado_por',
         'habilitado_por'
     ];
 
+    // Dates
     protected $useTimestamps = false;
-    protected $updatedField  = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
 }
