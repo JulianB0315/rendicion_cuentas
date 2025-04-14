07c94cc398d16d5ab0047bd69a5d3b6d717b8a22
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2025-04-14 18:46:48 -0500

    Update EjeModel.php

--- app/Models/EjeModel.php
+++ app/Models/EjeModel.php
@@ -5,13 +5,38 @@ namespace App\Models;
 class EjeModel extends \CodeIgniter\Model
 {
     protected $table = 'eje';
-    protected $primaryKey = 'id_eje';
+    protected $primaryKey = 'id';
     protected $useAutoIncrement = false;
     protected $returnType = 'array';
     protected $useSoftDeletes = false;
-    protected $allowedFields = ['id_eje', 'tematica'];
+    protected $allowedFields =
+    [
+        'id',
+        'tematica'
+    ];
 
+    // Dates
     protected $useTimestamps = false;
-    protected $updatedField  = false;
-    
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
 }
