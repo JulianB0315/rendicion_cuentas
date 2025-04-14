d43caaba3e49587a47498bd85d0e3b2817d97b10
Diego17 <diegoalbertocastropastor@gmail.com>
2025-04-14 17:25:20 -0500

    Add appHelpers.js script to form and login_admin views for improved functionality

--- app/Views/form.php
+++ app/Views/form.php
@@ -277,6 +277,7 @@
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<script src="<?= base_url("js/appHelpers.js") ?>"></script>
 	<script src="<?= base_url("js/form.js") ?>"></script>
 	<script src="<?= base_url('js/alerts.js') ?>"></script>
 </body>
