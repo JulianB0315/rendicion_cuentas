b2c383cd571462b0068a8700d3a97b2b29d24026
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2025-04-14 18:46:50 -0500

    Update Ejes_SeleccionadosModel.php

--- app/Models/Ejes_SeleccionadosModel.php
+++ app/Models/Ejes_SeleccionadosModel.php
@@ -5,18 +5,39 @@ namespace App\Models;
 class Ejes_SeleccionadosModel extends \CodeIgniter\Model
 {
     protected $table = 'ejes_seleccionados';
-    protected $primaryKey = 'id_eje_seleccionado';
+    protected $primaryKey = 'id';
     protected $useAutoIncrement = false;
     protected $returnType = 'array';
     protected $useSoftDeletes = false;
-    protected $allowedFields = [
-        'id_eje_seleccionado',
+    protected $allowedFields = 
+    [
+        'id',
         'id_rendicion',
         'id_eje',
         'cantidad_preguntas'
     ];
-
-
+    // Dates
     protected $useTimestamps = false;
-    protected $updatedField  = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
 }
