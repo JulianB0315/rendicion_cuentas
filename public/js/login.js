fd21ab6bf9fa0b76f63bf8b7cf1db3bc4513ec8d
Diego17 <diegoalbertocastropastor@gmail.com>
2025-04-14 19:23:04 -0500

    Refactor password visibility toggle logic: simplify event listener implementation

--- public/js/login.js
+++ public/js/login.js
@@ -77,14 +77,5 @@ dniInput.addEventListener("input", () => {
 
 // Mostrar/ocultar contraseña
 btnToggle.addEventListener("click", () => {
-    const icon = btnToggle.querySelector("i");
-    if (passwordInput.type === "password") {
-        passwordInput.type = "text";
-        icon.classList.add("fa-eye");
-        icon.classList.remove("fa-eye-slash");
-    } else {
-        passwordInput.type = "password";
-        icon.classList.add("fa-eye-slash");
-        icon.classList.remove("fa-eye");
-    }
+    togglePasswordVisibility(passwordInput, btnToggle);
 });
