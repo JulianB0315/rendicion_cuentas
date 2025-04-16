0de18626ef6358efe526585f1a5582d1c0c0f922
Julian Burga Bracamonte <julianburgabracamonte@gmail.com>
2025-04-16 11:06:32 -0500

    Update admin_users.php

--- app/Views/rendicion_cuentas/Admin/admin_users.php
+++ app/Views/rendicion_cuentas/Admin/admin_users.php
@@ -33,18 +33,35 @@
 <body>
     <header class="container-fluid header p-3 mb-5">
         <nav class="nav-header container">
-            <div class="d-flex align-items-center logo-container w-100 justify-content-between gap-3">
+            <div class="d-flex align-items-center logo-container w-100 justify-content-between">
                 <img
                     src="<?= base_url('img/logo.png') ?>"
                     alt="Logo"
                     class="nav-logo img-fluid" />
-                <h4 class="header-welcome text-center">Bienvenid@, <?= esc($nombre) ?></h4>
-                <a
-                    class="text-white register-btn btn p-md-3 p-sm-1 btn-header-register"
-                    href="<?= RUTA_ADMIN_HISTORIAL ?>"
-                    title="Ver tu historial">
-                    Ver historial
-                </a>
+                <div class="links-container">
+                    <ul class="list-unstyled d-flex align-items-center justify-content-evenly links-list">
+                        <li>
+                            <div class="dropdown">
+                                <button class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Acciones
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li><a href="<?= RUTA_ADMIN_BUSCAR_RENDICION ?>questions" class="dropdown-item">Seleccionar Preguntas</a></li>
+                                    <li><a href="<?= RUTA_ADMIN_BUSCAR_RENDICION ?>viewQuestions" class="dropdown-item">Ver preguntas</a></li>
+                                    <li><a href="<?= RUTA_ADMIN_BUSCAR_RENDICION ?>report" class="dropdown-item">Reportes</a></li>
+                                    <?php if (isset($categoria) && $categoria === 'super_admin'): ?>
+                                        <li>
+                                            <a href="<?= base_url('admin/admin_users') ?>" class="dropdown-item">Administrar Usuarios</a>
+                                        </li>
+                                    <?php endif; ?>
+                                    <li>
+                                        <a href="<?= base_url('logout') ?>" class="dropdown-item logout">Cerrar Sesión</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     </header>
