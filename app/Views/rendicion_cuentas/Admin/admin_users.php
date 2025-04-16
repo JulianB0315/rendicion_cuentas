66c024d176b65606e0ef4f0c4586eff3fc2c15b2
Diego17 <diegoalbertocastropastor@gmail.com>
2025-04-16 12:33:01 -0500

    Update admin_users.php to replace 'Reportes' option with 'Editar Rendición' in the actions dropdown menu

--- app/Views/rendicion_cuentas/Admin/admin_users.php
+++ app/Views/rendicion_cuentas/Admin/admin_users.php
@@ -52,10 +52,7 @@
                                     <li><a href="<?= RUTA_ADMIN_BUSCAR_RENDICION ?>questions" class="dropdown-item">Seleccionar Preguntas</a></li>
                                     <li><a href="<?= RUTA_ADMIN_BUSCAR_RENDICION ?>viewQuestions" class="dropdown-item">Ver preguntas</a></li>
                                     <li><a href="<?= RUTA_ADMIN_BUSCAR_RENDICION ?>report" class="dropdown-item">Reportes</a></li>
-
-                                    <li>
-                                        <a href="<?= base_url('admin/admin_users') ?>" class="dropdown-item">Administrar Usuarios</a>
-                                    </li>
+                                    <li><a href="<?= RUTA_ADMIN_BUSCAR_RENDICION ?>editarRendicion" class="dropdown-item">Editar Rendición</a></li>
                                     <li>
                                         <a
                                             class="dropdown-item"
