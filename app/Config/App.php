1320b5952e8fac404482a7edd5b74a7454c2a857
Julian Burga Bracamonte <julianburgabracamonte@gmail.com>
2025-04-16 11:06:16 -0500

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public $baseURL = 'https://localhost/rendicion_cuentas/';
+    public $baseURL = 'http://localhost/rendicion_cuentas/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
